b53a859be047d02e58f48c26d916608236b35afc
nowaday <nowaday@github.com>
2020-09-15 01:52:12 +0300

    upd setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh: long_description = fh.read()
 
 setuptools.setup(
-    name="spotify-api",
+    name="spotifyapi",
     version="0.1",
     author="lisp3r",
     author_email="none",
