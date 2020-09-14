2a2c0c2cf13aff2cca91404df747b249e050df0c
nowaday <nowaday@github.com>
2020-09-15 01:43:47 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ import setuptools
 with open("README.md", "r") as fh: long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-lisp3r",
+    name="spotify-api",
     version="0.1",
     author="lisp3r",
     author_email="none",
     description="Spotify API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/lisp3r/SpotifySmth",
-    packages=setuptools.find_packages(),
+    url="https://github.com/lisp3r/SpotifyAPI",
+    packages=["spotifyapi"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
+    include_package_data=True,
 )
